b9023cb6f5b043977941e4cf1b2a1cd1713dd41b
Kairos Tay <hestiatay@gmail.com>
2023-12-15 22:03:58 +0800

    chore: refac

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {Contact, Profile, Achievement, Experience, Education, Hero, Navbar, Tech, Project, StarsCanvas, Content, Footer } from "./components";
+import {
+  Contact,
+  Profile,
+  Achievement,
+  Experience,
+  Education,
+  Hero,
+  Navbar,
+  Tech,
+  Project,
+  StarsCanvas,
+  Content,
+  Footer,
+} from "./components";
 
 const App = () => {
-
   return (
     <div>
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <Content />
+          <Education />
+          <Project />
+          <Experience />
+          <Achievement />
+          <Profile />
+          <Tech />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+          <Footer />
         </div>
-        <Content />
-        <Education />
-        <Project />
-        <Experience />
-        <Achievement />
-        <Profile/>
-        <Tech />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
-        <Footer/>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
