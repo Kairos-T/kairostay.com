3fd2f283222e763274ff065719c42d3632612dd2
Kairos Tay <hestiatay@gmail.com>
2023-12-15 22:02:47 +0800

    feat: refactor

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
     build: {
         chunkSizeWarningLimit: 100000000
     },
-    base: "/3d-react-portfolio",
+    base: "/",
 })
