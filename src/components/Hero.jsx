eaf208d20cd68e607ddb6892cf783b5557ce843b
Kairos Tay <hestiatay@gmail.com>
2023-12-15 23:09:16 +0800

    feat: centered and cleaned up hero sect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useRef } from "react";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 import { init } from "ityped";
-import React, { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { AiOutlineGithub } from "react-icons/ai";
 import { ImLinkedin } from "react-icons/im";
 import { BiLinkAlt } from "react-icons/bi";
@@ -14,48 +15,48 @@ const Hero = () => {
     init(textRef.current, {
       showCursor: true,
       strings: [
-        "Cybersecurity Student",
-        "Aspiring Researcher",
-        "Forensics and Quantum Lover",
+        "A Cybersecurity Student",
+        "An Aspiring Researcher",
+        "A Forensics and Quantum Lover",
       ],
     });
   }, []);
 
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div className="flex">
-        <div
-          className={`head1 absolute  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-        >
-          <div className="flex flex-col justify-center items-center mt-5">
-          </div>
+    <section className={`relative w-full h-screen flex mx-auto`}>
+      <div
+        className={`head1 max-w-7xl mx-auto ${styles.paddingX} flex justify-center items-center flex-row gap-5`}
+      >
+        <div className="flex flex-col mt-5"></div>
 
-          <div className="head2">
-            <h1 className={`${styles.heroHeadText} text-white heroHeadText`}>
-              root@kairos:~# whoami
-            </h1>
-            <h3>
-              <span
-                ref={textRef}
-                className={`${styles.heroSubText} mt-2 green-text-gradient heroHeadText`}
-              ></span>
-            </h3>
-
-            <div className="absolute link1">
-              <a href="https://github.com/kairos-t" target="_blank">
-                <AiOutlineGithub />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/kairoshestiatay/"
-                target="_blank"
-              >
-                <ImLinkedin />
-              </a>
-
-              {/* <a href="https://linktr.ee/rathi17" target="_blank">
+        <div className="head2 text-center">
+          <h1 className={`${styles.heroHeadText} text-white heroHeadText`}>
+            root@kairos:~# whoami
+          </h1>
+          <h3 className="heroHeadText text-white" style={{fontSize: '2rem'}}>
+            Hi! I'm Kairos. I'm:
+          </h3>
+          <h3>
+            <span
+              ref={textRef}
+              className={`${styles.heroSubText} mt-2 green-text-gradient heroHeadText`}
+            ></span>
+          </h3>
+          <div className="link1 mt-4">
+            <a href="https://github.com/kairos-t" target="_blank">
+              <AiOutlineGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kairoshestiatay/"
+              target="_blank"
+            >
+              <ImLinkedin />
+            </a>
+            {
+              <a href="https://blog.kairostay.com" target="_blank">
                 <BiLinkAlt />
-              </a> */}
-            </div>
+              </a>
+            }
           </div>
         </div>
       </div>
