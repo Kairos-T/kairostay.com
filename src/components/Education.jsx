fa5aeade2c743769448c61b101ba0bb1f6965d5c
Kairos Tay <hestiatay@gmail.com>
2023-12-16 14:57:01 +0800

    Added education details

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -51,13 +51,13 @@ const FeedbackCard = ({
 
 const Education = () => {
   return (
-    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+    <div className={`Box mt-12 bg-black-100 rounded-[20px]`}>
       <div
         className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
       >
         <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>Education Details...</p>
-          <h2 className={styles.sectionHeadText}>Education.</h2>
+          <p className={styles.sectionSubText}>My</p>
+          <h2 className={styles.sectionHeadText}>Education</h2>
         </motion.div>
       </div>
       <div className={`-mt-20 justify-center pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
