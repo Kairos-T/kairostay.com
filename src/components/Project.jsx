44b5e27f6d6a8138853c7515de60f1700d12e563
Kairos Tay <hestiatay@gmail.com>
2023-12-16 20:48:15 +0800

    feat: changed tag format

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,12 +5,16 @@ import { styles } from "../styles";
 import { github } from "../assets";
 import { demo } from "../assets";
 import { SectionWrapper } from "../hoc";
-import {list} from "../constants"
+import { list } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
-import { cProject, javaProject, webProject, otherProject } from "../constants";
+import {
+  prgProject,
+  eventProject,
+  webProject,
+  researchProject,
+} from "../constants";
 import ProjectList from "./ProjectList";
 import "./Project.scss";
-
 
 const ProjectCard = ({
   index,
@@ -22,39 +26,44 @@ const ProjectCard = ({
   source_link,
 }) => {
   return (
-    <motion.div whileInView={{ opacity: 1 , transform : 'none'}} variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div
+      whileInView={{ opacity: 1, transform: "none" }}
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+    >
       <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className='project-box bg-additional p-5 rounded-2xl sm:w-[330px] w-full'
+        className="project-box bg-additional p-5 rounded-2xl sm:w-[330px] w-full"
       >
-        <div className='Box1 relative w-full h-[180px]'>
+        <div className="Box1 relative w-full h-[180px]">
           <img
             src={image}
-            alt='project_image'
-            className='image w-full h-full object-cover rounded-2xl'
+            alt="project_image"
+            className="image w-full h-full object-cover rounded-2xl"
           />
 
-        <div className='absolute inset-0 flex justify-center card-img_hover' style={{alignItems: "center",}}>
-          <h3 className=' font-bold text-[16px]'>{name}</h3>
+          <div
+            className="absolute inset-0 flex justify-center card-img_hover"
+            style={{ alignItems: "center" }}
+          >
+            <h3 className=" font-bold text-[16px]">{name}</h3>
           </div>
 
-          <div className='title absolute inset-0 flex justify-end card-img_hover'>
+          <div className="title absolute inset-0 flex justify-end card-img_hover">
             <div
               onClick={() => window.open(source_link, "_blank")}
-              className='black-gradient w-10 h-10 m-2 rounded-full flex justify-center items-center cursor-pointer'
+              className="black-gradient w-10 h-10 m-2 rounded-full flex justify-center items-center cursor-pointer"
             >
-
               <img
                 src={demo}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
-            <div
+            {/* <div
               onClick={() => window.open(source_code_link, "_blank")}
               className='black-gradient w-10 h-10 m-2 rounded-full flex justify-center items-center cursor-pointer'
             >
@@ -63,22 +72,26 @@ const ProjectCard = ({
                 alt='source code'
                 className='w-1/2 h-1/2 object-contain'
               />
-            </div>
-
-          </div>          
+            </div> */}
+          </div>
         </div>
 
-        <div className='content mt-5'>
-          <p className='mt-2 text-secondary text-[14px]' style={{textAlign:'justify'}}>{description}</p>
+        <div className="content mt-5">
+          <p
+            className="mt-2 text-secondary text-[14px]"
+            style={{ textAlign: "justify" }}
+          >
+            {description}
+          </p>
         </div>
 
-        <div className='content mt-4 flex flex-wrap gap-2'>
+        <div className="content mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
               className={`text-[14px] ${tag.color}`}
             >
-              #{tag.name}
+              ./{tag.name}
             </p>
           ))}
         </div>
@@ -87,65 +100,73 @@ const ProjectCard = ({
   );
 };
 const Project = () => {
-
-  const [selected, setSelected] = useState("java");
+  const [selected, setSelected] = useState("event");
   const [data, setData] = useState([]);
 
   useEffect(() => {
     switch (selected) {
-      case "java":
-        setData(javaProject);
+      case "event":
+        setData(eventProject);
         break;
-      case "c++":
-        setData(cProject);
+      case "prg":
+        setData(prgProject);
         break;
       case "web":
         setData(webProject);
         break;
-      case "other":
-        setData(otherProject);
+      case "research":
+        setData(researchProject);
         break;
 
       default:
-        setData(cProject);
+        setData(prgProject);
     }
   }, [selected]);
 
   return (
     <>
-      <motion.div whileInView={{ opacity: 1 , transform : 'none'}} variants={textVariant()}>
-        <p className={`${styles.sectionSubText} sectionHeadText text-center` }>root@kairos:~#</p>
-        <h2 className={`${styles.sectionHeadText} sectionHeadText text-center`}>./Events && ./Projects</h2>
+      <motion.div
+        whileInView={{ opacity: 1, transform: "none" }}
+        variants={textVariant()}
+      >
+        <p className={`${styles.sectionSubText} sectionHeadText text-center`}>
+          root@kairos:~#
+        </p>
+        <h2 className={`${styles.sectionHeadText} sectionHeadText text-center`}>
+          ./Events && ./Projects
+        </h2>
       </motion.div>
 
-      <div className='project w-full flex'>
-        <motion.p whileInView={{ opacity: 1 , transform : 'none'}}
+      <div className="project w-full flex">
+        <motion.p
+          whileInView={{ opacity: 1, transform: "none" }}
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] leading-[30px]'
+          className="mt-3 text-secondary text-[17px] leading-[30px]"
         >
-        <ul>
-        {list.map((item) => (
-          <ProjectList
-            title={item.title}
-            active={selected === item.id}
-            setSelected={setSelected}
-            id={item.id}
-          />
-        ))}
-      </ul>
+          <ul>
+            {list.map((item) => (
+              <ProjectList
+                title={item.title}
+                active={selected === item.id}
+                setSelected={setSelected}
+                id={item.id}
+              />
+            ))}
+          </ul>
 
-      <div className='box mt-20 flex flex-wrap justify-center'>
-        {data.map((project, index) => (
-          <div>
-            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <div className="box mt-20 flex flex-wrap justify-center">
+            {data.map((project, index) => (
+              <div>
+                <ProjectCard
+                  key={`project-${index}`}
+                  index={index}
+                  {...project}
+                />
+              </div>
+            ))}
           </div>
-        ))}
+        </motion.p>
       </div>
-
-
-      </motion.p>
-      </div>
-
     </>
   );
 };
