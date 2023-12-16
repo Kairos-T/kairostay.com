4b0e1fd0972368be0cd69662e055fe48346c3161
Kairos Tay <hestiatay@gmail.com>
2023-12-16 15:57:29 +0800

    temp: removed icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain logo" />
+          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain logo" /> */}
           <p className="sm:block text-white text-[18px] font-bold cursor-pointer flex ">
             root@kairos:~
           </p>
