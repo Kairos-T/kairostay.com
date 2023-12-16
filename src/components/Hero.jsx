ee5f008504f6266957b1b9187b2f9f495606f5a9
Kairos Tay <hestiatay@gmail.com>
2023-12-17 01:20:34 +0800

    fix: uncentered scrollbar since it gets covered by the bottom navbar :(

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white heroHeadText`}>
             root@kairos:~# whoami
           </h1>
-          <h3 className="heroHeadText text-white" style={{ fontSize: "2rem" }}>
+          <h3 className="heroHeadText text-white" style={{fontSize: '2rem'}}>
             Hi! I'm Kairos. I'm:
           </h3>
           <h3>
@@ -62,7 +62,7 @@ const Hero = () => {
       </div>
 
       {/* <ComputersCanvas /> */}
-      <div className="absolute xs:bottom-10 bottom-32 left-1/2 transform -translate-x-1/2 w-10 flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-10 flex justify-end items-center">
         <a href="#education">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
