d29658fb20cadf6745385dda5eb6f57cdab36dba
Kairos Tay <hestiatay@gmail.com>
2023-12-17 01:48:00 +0800

    chore: code refactoring

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
-    },
-    base: "/",
-})
+  build: {
+    chunkSizeWarningLimit: 100000000,
+  },
+  base: "/",
+});
