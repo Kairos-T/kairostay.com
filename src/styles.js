ea50bbb0b1696be1610f4fded7b15c0328586cce
Kairos Tay <hestiatay@gmail.com>
2023-12-16 15:55:33 +0800

    feat: removed force caps in subtexts

--- src/styles.js
+++ src/styles.js
@@ -11,7 +11,7 @@ const styles = {
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
-      "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+      "sm:text-[18px] text-[14px] text-secondary tracking-wider",
   };
   
   export { styles };
