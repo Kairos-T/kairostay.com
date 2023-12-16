817269d0e5d1d85c65ea2ca2834e6386ec185c0b
Kairos Tay <hestiatay@gmail.com>
2023-12-16 14:58:00 +0800

    feat: edited colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#343a40",
+        primary: "#1e1e1e",
         secondary: "#52F165",
-        tertiary: "#6c757d",
+        tertiary: "#696969",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: "0px 35px 120px -15px #343a40",
       },
       screens: {
         xs: "450px",
