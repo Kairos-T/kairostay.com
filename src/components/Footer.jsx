5aac9054f81f177834c044bd5810c32f2062a420
Kairos Tay <hestiatay@gmail.com>
2023-12-17 22:12:06 +0800

    chore: code reformatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,27 @@ import { useEffect, useState } from "react";
 import "./Footer.scss";
 
 const Footer = () => {
+  return (
+    <div className="footer text-center">
+      <p>
+        &#x3c;&#47;&#x3e; with ❤️ by
+        <a href="https://github.com/kairos-t" target="_blank">
+          {" "}
+          Kairos Tay
+        </a>
+      </p>
+      <p className="green-text-gradient">
+        No. of Visitors |{" "}
+        <img
+          className="visitcounter"
+          src="https://www.cutercounter.com/hits.php?id=hvmxopodq&nd=6&style=16"
+          title="Counter Widget"
+          Alt="Visit counter For Websites"
+          border="0"
+        />
+      </p>
+    </div>
+  );
+};
 
-    return (
-        <div className="footer text-center">
-        <p> 
-          &#x3c;&#47;&#x3e; with ❤️ by
-          <a href="https://github.com/kairos-t" target="_blank">
-            {" "}
-            Kairos Tay
-          </a>
-        </p>
-        <p className="green-text-gradient">No. of Visitors | <img className="visitcounter" src="https://www.cutercounter.com/hits.php?id=hvmxopodq&nd=6&style=16" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></p>
-
-      </div>
-    );
-  };
-  
-  export default Footer;
+export default Footer;
