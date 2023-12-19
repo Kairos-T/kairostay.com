8ade51a0bd34b56734106be6a90ed6763c7b24e4
Kairos Tay <hestiatay@gmail.com>
2023-12-19 15:53:22 +0800

    feat: added beating heart using fa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./Footer.scss";
+
+config.autoAddCss = false;
 
 const Footer = () => {
   return (
     <div className="footer text-center">
       <p>
-        &#x3c;&#47;&#x3e; with ❤️ by
+        &#x3c;&#47;&#x3e; with{" "}
+        <FontAwesomeIcon
+          icon={faHeart}
+          className="fa-beat"
+          style={{
+            "--fa-animation-duration": "0.8s",
+            color: "#f54545",
+          }}
+        />{" "}
+        by
         <a href="https://github.com/kairos-t" target="_blank">
           {" "}
           Kairos Tay
@@ -18,7 +33,6 @@ const Footer = () => {
           className="visitcounter"
           src="https://hitwebcounter.com/counter/counter.php?page=10365307&style=0025&nbdigits=5&type=page&initCount=100"
           title="Counter Widget"
-          Alt="Visit counter For Websites"
           border="0"
         />
       </p>
