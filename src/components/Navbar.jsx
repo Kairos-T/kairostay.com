727d9d90907503024839cdc39d337d37f89ed18e
Kairos Tay <hestiatay@gmail.com>
2023-12-22 00:02:28 +0800

    feat: hamburger menu for responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import { styles } from "../styles";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [scrolled, setScrolled] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,9 +19,20 @@ const Navbar = () => {
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    // Initial check for mobile view
+    handleResize();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   const scrollToSection = (sectionId) => {
@@ -31,85 +43,190 @@ const Navbar = () => {
     }
   };
 
+  const toggleMobileMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-3 fixed top-0 z-20 ${
-        scrolled ? "translucent" : "bg-transparent "
-      }`}
-    >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <div className="flex items-center gap-5">
-          <div
-            className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer text-decoration-none`}
-          >
-            {" "}
-            <a
-              href="#experience"
-              className={`nav-link ns-blue ${
-                active === "experience" ? "active" : ""
-              }`}
-              onClick={() => scrollToSection("experience")}
-            >
-              ./Experience
-            </a>
-          </div>
-
-          <div
-            className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer text-decoration-none`}
-          >
-            {" "}
-            <a
-              href="#project"
-              className={`nav-link ${active === "project" ? "active" : ""}`}
-              onClick={() => scrollToSection("project")}
-            >
-              ./Projects
-            </a>
-          </div>
-        </div>
-
-        <a
-          href="#"
-          className="flex items-center gap-2"
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
+    <>
+      {/* Mobile View */}
+      {isMobile && (
+        <div
+          className={`${
+            styles.paddingX
+          } w-full flex items-center py-3 fixed top-0 z-20 ${
+            scrolled ? "translucent" : "bg-transparent"
+          }`}
         >
           <p className="sm:block text-white text-[18px] font-bold cursor-pointer flex ">
             root@kairos:~
           </p>
-        </a>
-
-        <div className="sm:flex gap-5 ">
-          <div
-            className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer text-decoration-none`}
-          >
-            <a
-              href="#contact"
-              className={` ${active === "contact" ? "active" : ""}`}
-              onClick={() => scrollToSection("contact")}
+          <div className="cursor-pointer ml-auto" onClick={toggleMobileMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6 text-white"
             >
-              ./Contact
-            </a>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
           </div>
+          {menuOpen && (
+            <div className="absolute top-16 right-0 translucent shadow-md">
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer p-2`}
+              >
+                <a
+                  href="#experience"
+                  className={`nav-link ns-blue ${
+                    active === "experience" ? "active" : ""
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection("experience");
+                    toggleMobileMenu();
+                  }}
+                >
+                  ./Experience
+                </a>
+              </div>
 
-          <div
-            className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer`}
-          >
-            <a
-              href="/files/resume/Resume.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              &gt; Resume
-            </a>
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer p-2`}
+              >
+                <a
+                  href="#project"
+                  className={`nav-link ${active === "project" ? "active" : ""}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection("project");
+                    toggleMobileMenu();
+                  }}
+                >
+                  ./Projects
+                </a>
+              </div>
+
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer p-2`}
+              >
+                <a
+                  href="#contact"
+                  className={` ${active === "contact" ? "active" : ""}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection("contact");
+                    toggleMobileMenu();
+                  }}
+                >
+                  ./Contact
+                </a>
+              </div>
+
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer p-2`}
+              >
+                <a
+                  href="/files/resume/Resume.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  &gt; Resume
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+
+      {/* Desktop View */}
+      {!isMobile && (
+        <div
+          className={`${
+            styles.paddingX
+          } w-full flex items-center py-3 fixed top-0 z-20 ${
+            scrolled ? "translucent" : "bg-transparent"
+          }`}
+        >
+          <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+            <div className="flex items-center gap-5">
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer text-decoration-none`}
+              >
+                <a
+                  href="#experience"
+                  className={`nav-link ns-blue ${
+                    active === "experience" ? "active" : ""
+                  }`}
+                  onClick={() => {
+                    scrollToSection("experience");
+                    toggleMobileMenu();
+                  }}
+                >
+                  ./Experience
+                </a>
+              </div>
+
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer text-decoration-none`}
+              >
+                <a
+                  href="#project"
+                  className={`nav-link ${active === "project" ? "active" : ""}`}
+                  onClick={() => {
+                    scrollToSection("project");
+                    toggleMobileMenu();
+                  }}
+                >
+                  ./Projects
+                </a>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <p className="sm:block text-white text-[18px] font-bold cursor-pointer flex ">
+                root@kairos:~
+              </p>
+            </div>
+
+            <div className="sm:flex gap-5">
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer text-decoration-none`}
+              >
+                <a
+                  href="#contact"
+                  className={` ${active === "contact" ? "active" : ""}`}
+                  onClick={() => {
+                    scrollToSection("contact");
+                    toggleMobileMenu();
+                  }}
+                >
+                  ./Contact
+                </a>
+              </div>
+
+              <div
+                className={`top2 ${"text-secondary"} hover:text-white text-[15px] font-medium cursor-pointer`}
+              >
+                <a
+                  href="/files/resume/Resume.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  &gt; Resume
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      )}
+    </>
   );
 };
 
