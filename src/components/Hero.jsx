526ae3b31c82b6912e9068bf521526acd65a2663
Kairos-T <hestiatay@gmail.com>
2024-03-30 22:58:15 +0800

    fix: removed buggy arrow (the one that leads to #experience)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,26 +51,6 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
-
-            <div
-                className="absolute xs:bottom-3 s:bottom-13 bottom-20 md:bottom-32 w-full flex justify-center items-center">
-                <a href="#experience">
-                    <div
-                        className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-                        <motion.div
-                            animate={{
-                                y: [0, 24, 0],
-                            }}
-                            transition={{
-                                duration: 1.5,
-                                repeat: Infinity,
-                                repeatType: "loop",
-                            }}
-                            className="w-3 h-3 rounded-full bg-secondary mb-1"
-                        />
-                    </div>
-                </a>
-            </div>
         </section>
     );
 };
